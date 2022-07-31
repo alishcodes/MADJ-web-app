ce5d588e9ce68f22da45bd4180c691d00a8712c8
Navarro-Jonathan <navarrojonathanj@gmail.com>
2022-07-31 02:10:23 -0700

    Split string for ENV variables

--- Java/MADJ/src/main/java/Driver.java
+++ Java/MADJ/src/main/java/Driver.java
@@ -5,10 +5,13 @@ public class Driver
     public static void main (String[] args)
     {
         Connection conn = null;
+        String DBPass = System.getenv("DBPASS");
+        String DBInstanceName = System.getenv("DBINSTANCENAME");
+        String DBName = System.getenv("DBNAME");
 
         try
         {
-            String url = "jdbc:mysql:///MADJSiteDB?cloudSqlInstance=nth-segment-357600:us-west1:madj-01&socketFactory=com.google.cloud.sql.mysql.SocketFactory&user=root&password=G23FDSZhre4";
+            String url = String.format("jdbc:mysql:///%s?cloudSqlInstance=%s&socketFactory=com.google.cloud.sql.mysql.SocketFactory&user=root&password=%s", DBName, DBInstanceName, DBPass);
             Class.forName ("com.mysql.cj.jdbc.Driver");
             conn = DriverManager.getConnection(url);
             System.out.println ("Database connection established");
