9e4e011a8b54a67bda1c5287814741501cf868bd
Alisha A <alisha.arabatlian1997@gmail.com>
2022-08-03 00:07:57 -0700

    Made drop down menu for Clothing button.

--- front-end/src/MenuBar.js
+++ front-end/src/MenuBar.js
@@ -1,24 +1,33 @@
 import React from "react";
-import { IconButton, Typography, Stack, AppBar, Drawer, Box, List, ListItem, ListItemButton, ListItemText, ListItemIcon, Grid } from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { 
+    IconButton, Typography, Stack, AppBar, 
+    Drawer, Box, Button, List, ListItem, ListItemButton, 
+    ListItemText, ListItemIcon, Grid, Divider } from "@mui/material";
+import Collapse from '@mui/material/Collapse';
+import { Menu, ShoppingCart, ExpandLess, ExpandMore } from '@mui/icons-material';
 import Toolbar from "@mui/material/Toolbar";
 import { Link } from 'react-router-dom';
 
 const MenuBar = () => {
     {/*Initialize menuOpen to false and func to set state*/}
     const [menuOpen, setMenuOpen] = React.useState(false);
+    const [openSubMenu, setOpenSubMenu] = React.useState(false);
 
     {/*Open = boolean, sets menuOpen state*/}
     const toggleDrawer = (open) => {
         setMenuOpen(open);
     }
 
+    const toggleSubMenu = () => {
+        setOpenSubMenu((!openSubMenu));
+    }
+
     const menuItems = [
+        { link: "/", text: 'All' },
         { link: "/", text: 'Tops' },
-        { link: "/", text: 'Bottoms'},
-        { link: "/", text: 'Shoes'},
-        { link: "/", text: 'Accessories'}
+        { link: "/", text: 'Outerwear'},
+        { link: "/", text: 'Shorts'},
+        { link: "/", text: 'Pants'}
     ];
 
     return (
@@ -31,7 +40,7 @@ const MenuBar = () => {
                         justifyContent="space-between"
                         alignItems="center">
                         <IconButton size="large" edge="start" onClick={() => toggleDrawer(true)}> {/*When clicked, open=true*/}
-                            <MenuIcon sx={{ color: "gray" }}/>
+                            <Menu sx={{ color: "gray" }}/>
                         </IconButton>
                         <Link to={"/"} style={{ textDecoration: 'inherit', color: 'inherit' }}>
                             <Typography variant="overline" fontSize={30} sx={{ color: "gray" }}>
@@ -40,7 +49,7 @@ const MenuBar = () => {
                         </Link>
                         <Link to={"/"} style={{ textDecoration: 'inherit', color: 'inherit' }}>
                             <IconButton size="large">
-                                <ShoppingCartIcon sx={{ color: "gray"}}/>
+                                <ShoppingCart sx={{ color: "gray"}}/>
                             </IconButton>
                         </Link>
                     </Stack>
@@ -55,17 +64,46 @@ const MenuBar = () => {
                     role="presentation"
                     sx={{ width: 250}}
                 >
-                    <List>
-
-                        {menuItems.map( ( curr, index ) => (
-                            <Link to={curr.link} style={{ textDecoration: 'inherit', color: 'inherit' }}>
-                                <ListItem disablePadding>
-                                    <ListItemButton onClick={() => toggleDrawer(false)}>
-                                        <ListItemText primary={curr.text} />
-                                    </ListItemButton>
-                                </ListItem>
-                            </Link>
-                        ))}
+                    <List component="nav" >
+                        <ListItem>
+                            <Stack direction="row" justifyContent="space-between" alignItems="center" flex={1}>
+                                <ListItemButton>
+                                    <ListItemText primary={"Clothing"}/>
+                                </ListItemButton>
+                                <Button onClick={toggleSubMenu} color="inherit">
+                                    {openSubMenu ? <ExpandLess/> : <ExpandMore/>}
+                                </Button>
+                            </Stack>
+                        </ListItem>
+                        <Collapse in={openSubMenu} unmountOnExit> {/*Show children when subMenuOpen = true*/}
+                            <Divider/>
+                            <List component="div" disablePadding>
+                                {menuItems.map( ( curr, index ) => (
+                                    <Link to={curr.link} style={{ textDecoration: 'inherit', color: 'inherit' }}>
+                                        <ListItem>
+                                            <ListItemButton onClick={() => toggleDrawer(false)} sx={{ pl: 4 }}>
+                                                <ListItemText primary={curr.text} />
+                                            </ListItemButton>
+                                        </ListItem>
+                                    </Link>
+                                ))}
+                            </List>
+                            <Divider/>
+                        </Collapse>
+                        <Link to="/" style={{ textDecoration: 'inherit', color: 'inherit' }}>
+                            <ListItem>
+                                <ListItemButton onClick={() => toggleDrawer(false)}>
+                                    <ListItemText primary={"Shoes"}/>
+                                </ListItemButton>  
+                            </ListItem>
+                        </Link>
+                        <Link to="/" style={{ textDecoration: 'inherit', color: 'inherit' }}>
+                            <ListItem>
+                                <ListItemButton onClick={() => toggleDrawer(false)}>
+                                    <ListItemText primary={"Accessories"}/>
+                                </ListItemButton>  
+                            </ListItem>
+                        </Link>
                     </List>
                 </Box>
             </Drawer>
