a5f24b8ebf49378f75d89e15aad65528db0ea874
Alisha A <56981295+alishcodes@users.noreply.github.com>
2022-08-04 14:30:01 -0700

    Created Catalog component page

---
+++ front-end/src/Catalog.js
@@ -0,0 +1,12 @@
+{/*Page that displays products*/}
+const Catalog = () => {
+    return (
+        <div>
+            <h1>
+                Catalog Title Page
+            </h1>
+        </div>
+    );
+}
+
+export default Catalog;
