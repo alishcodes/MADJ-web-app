01b67358d01b2382d24c5bb932673c46388909ba
Alisha A <56981295+alishcodes@users.noreply.github.com>
2022-08-07 15:33:07 -0700

    Updated the spacing of the catalog page

--- front-end/src/Catalog.js
+++ front-end/src/Catalog.js
@@ -1,4 +1,4 @@
-import { Grid, Box } from '@mui/material';
+import { Grid, Container } from '@mui/material';
 import ItemCard from './Components/ItemCard';
 
 /**
@@ -14,18 +14,20 @@ const onAddToCartClick = (id) => {
 /*Page that displays products*/
 const Catalog = () => {
     return (
-        <Grid container justifyContent="center" spacing={12} padding="50px">
-            {Array.from(Array(25)).map((currItem, index) => (
-                <Grid item xs={12} sm={6} md={4}>
-                    <ItemCard title={"Product Title"}
-                              img={"https://www.uniqlo.com/jp/ja/contents/feature/masterpiece/common_22ss/img/products/productsArea_itemimg_16_m.jpg?220211"}
-                              desc={"The product description will go here. The product description will go here."}
-                              price={10.99}
-                              onAddToCart={() => onAddToCartClick(index)}
-                    />
-                </Grid>
-            ))}
-        </Grid>
+        <Container>
+            <Grid container spacing={8} paddingTop="100px" maxWidth="1500px">
+                {Array.from(Array(25)).map((currItem, index) => (
+                    <Grid item xs={12} sm={6} md={4}>
+                        <ItemCard title={"Product Title"}
+                                  img={"https://www.uniqlo.com/jp/ja/contents/feature/masterpiece/common_22ss/img/products/productsArea_itemimg_16_m.jpg?220211"}
+                                  desc={"The product description will go here. The product description will go here."}
+                                  price={10.99}
+                                  onAddToCart={() => onAddToCartClick(index)}
+                        />
+                    </Grid>
+                ))}
+            </Grid>
+        </Container>
     );
 }
 
