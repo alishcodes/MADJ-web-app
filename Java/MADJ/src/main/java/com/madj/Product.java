b7300864ad64359af7bb3f779745ee005bbddeaf
Navarro-Jonathan <navarrojonathanj@gmail.com>
2022-08-10 11:18:39 -0700

    Override Product toString and Equals

--- Java/MADJ/src/main/java/com/madj/Product.java
+++ Java/MADJ/src/main/java/com/madj/Product.java
@@ -1,20 +1,25 @@
 package com.madj;
 
+import java.util.Objects;
+
 public class Product
 {
+    public enum ProductType{
+        Shirt,
+        Pants
+    }
     int id = 0;
     float price = 0;
     String title = " ", desc = " ", img = " ";
-    int type = 0;
-
+    ProductType productType;
     public Product(int id, float price, String title, String desc, String img,
-                   int type){
+                   ProductType productType){
         this.id = id;
         this.price = price;
         this.title = title;
         this.desc = desc;
         this.img = img;
-        this.type = type;
+        this.productType = productType;
     }
 
     public float getPrice(){
@@ -37,6 +42,29 @@ public class Product
         String temp = "";
         return temp;
     }
+    public ProductType getProductType(){
+        return productType;
+    }
+    @Override
+    public boolean equals(Object o) {
 
+        if (this == o)
+            return true;
+        if (!(o instanceof Product))
+            return false;
+        Product product = (Product) o;
+        return Objects.equals(this.id, product.id)
+                && Objects.equals(this.title, product.title)
+                && Objects.equals(this.desc, product.desc)
+                && Objects.equals(this.price, product.price)
+                && Objects.equals(this.productType, product.productType)
+                && Objects.equals(this.img, product.img);
+    }
 
+    public String toString(){
+        return "Product{" + "id=" + this.id + ", title='" + this.title
+                + '\'' + ", desc='" + this.desc + '\''
+                + ", price=" + this.price + ", type="+this.productType.ordinal()
+                + ", img='" + this.img + '\'' + '}';
+    }
 }
