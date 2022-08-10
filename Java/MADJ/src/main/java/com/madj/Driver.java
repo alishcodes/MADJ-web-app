f0c0730a1b7071c686ca180d7c876f7746786ee3
Navarro-Jonathan <navarrojonathanj@gmail.com>
2022-08-10 14:03:04 -0700

    Remove unused lines

--- Java/MADJ/src/main/java/com/madj/Driver.java
+++ Java/MADJ/src/main/java/com/madj/Driver.java
@@ -4,8 +4,6 @@ import java.sql.*;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -26,11 +24,5 @@ public class Driver
             }
             catch (Exception e) { }
         }
-    }
-
-
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
     }
 }
