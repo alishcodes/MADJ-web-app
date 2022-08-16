41a7ef1f14db72631f4578d96b00bafd26799524
Navarro-Jonathan <navarrojonathanj@gmail.com>
2022-08-16 14:17:31 -0700

    Update driver javadoc description

--- Java/MADJ/src/main/java/com/madj/Driver.java
+++ Java/MADJ/src/main/java/com/madj/Driver.java
@@ -6,8 +6,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 /**
- * <h1>MADJ Server</h1>
- * The server backend to the MADJ website.
+ * Server entry point.
+ * <p><h1>MADJ Server</h1></p>
+ * <p>The server backend to the MADJ website.</p>
  * <p>
  * <b>Note:</b> This server uses localhost port 8080 by default.
  *
