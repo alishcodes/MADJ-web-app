9d93e527a666385a9e613eba13779785f3513179
Alisha A <56981295+alishcodes@users.noreply.github.com>
2022-08-18 18:28:23 -0700

    Created tests for Mailer and Order classes

---
+++ Java/MADJ/src/test/java/com/madj/MailerTest.java
@@ -0,0 +1,12 @@
+package com.madj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MailerTest {
+
+    @Test
+    void send() {
+    }
+}
---
+++ Java/MADJ/src/test/java/com/madj/OrderTest.java
@@ -0,0 +1,36 @@
+package com.madj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTest {
+
+    @Test
+    void getBillingName() {
+    }
+
+    @Test
+    void getBillingAddress() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void getCustomerName() {
+    }
+
+    @Test
+    void getCardInfo() {
+    }
+
+    @Test
+    void getProductsInfo() {
+    }
+
+    @Test
+    void getTotal() {
+    }
+}
