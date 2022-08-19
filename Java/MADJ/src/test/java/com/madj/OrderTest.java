cc997a71c716d76431f4678fb45eda2880d5eab2
Navarro-Jonathan <navarrojonathanj@gmail.com>
2022-08-18 20:14:25 -0700

    Add 2 example products to OrderTest

--- Java/MADJ/src/test/java/com/madj/OrderTest.java
+++ Java/MADJ/src/test/java/com/madj/OrderTest.java
@@ -2,6 +2,7 @@ package com.madj;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.util.Assert;
 
 import java.util.HashMap;
 
@@ -13,7 +14,11 @@ class OrderTest {
     @BeforeEach
     void setUp(){
         HashMap<Product, Integer> products = new HashMap<Product, Integer>();
-        //TODO feed products fake Product instances
+        // TODO feed order some more fake Product instances and update orderTotal assertion accordingly
+        products.put(new Product(0, 1234, "A blue shirt", "This shirt is blue",
+                "123@imgur.com", Product.ProductType.Tops), 4);
+        products.put(new Product(1, 2345, "Red pants", "These pants are red",
+                "425@imgur.com", Product.ProductType.Pants), 3);
 
         order = new Order("Joe", "123 street", "Jack Nihoff",
                 "example@gmail.com", "12345678", products);
@@ -45,11 +50,34 @@ class OrderTest {
 
     @Test
     void getProductsInfo() {
-        // TODO implement this
+        // check each ProductInformation variable in the list to see that they were generated correctly
+        for(Order.ProductInformation prod : order.getProductsInfo()){
+            switch (prod.id){
+                case 0:
+                    assertEquals(0, prod.id);
+                    assertEquals(4, prod.quantity);
+                    assertEquals(1234, prod.price);
+                    assertEquals("A blue shirt", prod.title);
+                    assertEquals("This shirt is blue", prod.desc);
+                    assertEquals("123@imgur.com", prod.img);
+                    break;
+                case 1:
+                    assertEquals(1, prod.id);
+                    assertEquals(3, prod.quantity);
+                    assertEquals(2345, prod.price);
+                    assertEquals("Red pants", prod.title);
+                    assertEquals("These pants are red", prod.desc);
+                    assertEquals("425@imgur.com", prod.img);
+                    break;
+                default:
+                    fail("Index " + prod.id + " is not expected");
+            }
+        }
+
     }
 
     @Test
     void getTotal() {
-        // TODO implement this
+        assertEquals((1234 * 4) + (2345 * 3), order.getTotal());
     }
 }
